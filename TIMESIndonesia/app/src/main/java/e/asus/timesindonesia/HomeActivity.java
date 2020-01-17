a47c623fed9941414b171861849a5d753bccc58d
dyas irvan <dyas.mail@gmail.com>
2020-01-17 18:31:45 +0700

    home fixing

--- TIMESIndonesia/app/src/main/java/e/asus/timesindonesia/HomeActivity.java
+++ TIMESIndonesia/app/src/main/java/e/asus/timesindonesia/HomeActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.design.widget.BottomSheetDialog;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,20 +15,32 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
+import e.asus.timesindonesia.adapter.TrendingViewAdapter;
+import e.asus.timesindonesia.model.trending;
+
 public class HomeActivity extends Fragment implements View.OnClickListener {
-    private TextView mTextMessage;
-    private CardView card1;
-    public static final String API_KEY = "AIzaSyCe6tORd9Ch4lx-9Ku5SQ476uS9OtZYsWA";
-    public static final String VIDEO_ID = "b1cyu33SNL8";
     RelativeLayout relativeLayout;
     LinearLayout set, fav;
     BottomSheetDialog bottomSheetDialog;
     View view;
 
+    private TrendingViewAdapter adapter;
+    private String[] dataJudul;
+    private String[] dataDate;
+    private String[] dataKategori;
+    private String[] dataIsi;
+    private String[] dataNomer;
+    private String[] dataImg;
+    private ArrayList<trending> trendings = new ArrayList<>();
+    private RecyclerView rvTrending;
+    private LinearLayoutManager linearLayoutManager;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.activity_home, container, false);
-        ImageButton btn_menu = (ImageButton) view.findViewById(R.id.btn_menu);
+        ImageButton btn_menu = view.findViewById(R.id.btn_menu);
         btn_menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,14 +50,12 @@ public class HomeActivity extends Fragment implements View.OnClickListener {
         });
         createBottomSheetDialog();
 
-        card1 = view.findViewById(R.id.card1);
-        card1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent newsdetail = new Intent(getActivity(), NewsDetailActivity.class);
-                startActivity(newsdetail);
-            }
-        });
+        rvTrending = view.findViewById(R.id.rv_trending);
+        rvTrending.setHasFixedSize(true);
+//        rvTrending.setNestedScrollingEnabled(false);
+        trendings.addAll(getListMovies());
+        showRecyclerList();
+
         return view;
     }
 
@@ -74,5 +86,45 @@ public class HomeActivity extends Fragment implements View.OnClickListener {
                 bottomSheetDialog.dismiss();
                 break;
         }
+    }
+
+    public ArrayList<trending> getListMovies() {
+        dataJudul = getResources().getStringArray(R.array.data_judul);
+        dataDate = getResources().getStringArray(R.array.data_tanggal);
+        dataKategori = getResources().getStringArray(R.array.data_kategori);
+        dataNomer = getResources().getStringArray(R.array.data_nomer);
+        dataImg = getResources().getStringArray(R.array.data_img);
+        dataIsi = getResources().getStringArray(R.array.data_isi);
+        ArrayList<trending> listMovie = new ArrayList<>();
+        for (int i = 0; i < dataJudul.length; i++) {
+            trending movie = new trending();
+            movie.setJudul(dataJudul[i]);
+            movie.setTgl(dataDate[i]);
+            movie.setKategori(dataKategori[i]);
+            movie.setNomer(dataNomer[i]);
+            movie.setGmbr(dataImg[i]);
+            movie.setIsi(dataIsi[i]);
+            listMovie.add(movie);
+        }
+        return listMovie;
+    }
+
+    private void showRecyclerList(){
+        rvTrending.setLayoutManager(new LinearLayoutManager(
+                getActivity()));
+        adapter = new TrendingViewAdapter(trendings);
+        rvTrending.setAdapter(adapter);
+        adapter.setOnItemClickCallback(new TrendingViewAdapter.OnItemClickCallback() {
+            @Override
+            public void onItemClicked(trending data) {
+                showSelectedMovie(data);
+            }
+        });
+
+    }
+    private void showSelectedMovie(trending movie) {
+        Intent intent = new Intent(getActivity(), NewsDetailActivity.class);
+        intent.putExtra("detail",movie);
+        startActivity(intent);
     }
 }
